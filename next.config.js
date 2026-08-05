bf6a8a7c7be391b74b83caca69a04302210d3a53
jkyekye <jkyekye@nd.edu>
2026-08-05 15:29:05 -0400

    Fix GitHub Pages base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  basePath: isProduction ? "/jerroldkyekye.github.io" : "",
+  assetPrefix: isProduction ? "/jerroldkyekye.github.io/" : "",
   images: {
     unoptimized: true,
   },
